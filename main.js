94b3458113a12b1c7faa2a0b06e94006395070ed
Evandro Falleiros <evandro.falleiros@ifms.edu.br>
2025-05-19 23:43:42 -0400

    Corrige caminho de import do GLTFLoader

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { MindARThree } from 'mindar-image-three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Elementos da UI
 const startButton = document.getElementById('startButton');
